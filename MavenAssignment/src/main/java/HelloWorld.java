905ab4d96d37eb74058bb8d991682522169ecebf
lakshay.rawat@tothenew.com <lakshay.rawat@tothenew.com>
2022-02-24 23:54:51 +0530

    maven exercise push

---
+++ MavenAssignment/src/main/java/HelloWorld.java
@@ -0,0 +1,15 @@
+
+class Hello{
+    public void display(){
+        System.out.println("Hello World");
+    }
+}
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+        Hello h = new Hello();
+        h.display();
+    }
+
+}
