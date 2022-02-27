f34a874e2d1b3038fdeed19408c06704ecd88519
Lakshay Rawat <99396137+LakshayRawatTTN@users.noreply.github.com>
2022-02-27 20:25:15 +0530

    Update Account.java

--- Exercise6/deadlocks/Account.java
+++ Exercise6/deadlocks/Account.java
@@ -1,3 +1,7 @@
+//5) Write a program to simulate a deadlock in java and then improve
+// the program to handle the deadlock using reentract lock.
+
+
 package Exercise6.deadlocks;
 
 class Account {
